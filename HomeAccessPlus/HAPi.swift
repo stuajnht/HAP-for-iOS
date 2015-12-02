e9dc0bb9634715d5c2a1fec713afcbc06c2b152e
stuajnht <stuajnht@users.noreply.github.com>
2015-12-02 21:36:36 +0000

    Added HAPi class and started to create the checkAPI function

---
+++ HomeAccessPlus/HAPi.swift
@@ -0,0 +1,57 @@
+// Home Access Plus+ for iOS - A native app to access a HAP+ server
+// Copyright (C) 2015  Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+//
+//  HAPi.swift
+//  HomeAccessPlus
+//
+
+import Foundation
+import Alamofire
+
+/// HAPi class to format, get and return data from the HAP+ API
+///
+/// The main class that is to be used to access the HAP+ API on the
+/// remote HAP+ server. All functions in the app that need to access
+/// the API on the HAP+ server should send their requests to this
+/// class, so that all API calls are standardised and central.
+///
+/// Oh, and the reason for the name of this class? Well...
+/// * HAP(i) - Home Access Plus+
+/// * (H)APi - Application Programming Interface
+/// * HAPi - Who doesn't want to be HAPi?
+///   * The small 'i'? Well, why not?
+///
+/// - author: Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+/// - since: 0.2.0-alpha
+class HAPi {
+    
+    /// Checks to make sure that the HAP+ server is available and is able to
+    /// connect to the API
+    ///
+    /// All HAP+ servers have the ability to check that the API is available
+    /// to use. This check is located at "<HAP server URL>/api/test" and on
+    /// result of a successful API test the server returns "OK"
+    ///
+    /// - author: Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+    /// - since: 0.2.0-alpha
+    ///
+    /// - parameter hapServer: The full URL to the main HAP+ root
+    /// - returns: Can the HAP+ server API be contacted
+    func checkAPI(hapServer: String) -> Bool {
+        return false
+    }
+}
