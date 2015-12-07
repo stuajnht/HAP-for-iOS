507f5db222851e2cefcfc1bc25c6502875880d99
stuajnht <stuajnht@users.noreply.github.com>
2015-12-07 20:28:43 +0000

    Added JSON encoding to the HTTP request for the logon API, and also added an additional '?' to the URL being used

--- HomeAccessPlus/HAPi.swift
+++ HomeAccessPlus/HAPi.swift
@@ -131,7 +131,7 @@ class HAPi {
             ]
             
             // Connecting to the API to log in the user with the credentials
-            Alamofire.request(.POST, hapServer + "/api/ad/", parameters: ["username": username, "password": password], headers: httpHeaders)
+            Alamofire.request(.POST, hapServer + "/api/ad/?", parameters: ["username": username, "password": password], headers: httpHeaders, encoding: .JSON)
                 .responseJSON { response in
                     logger.debug("Response JSON: \(response.result.value)")
                     callback(false)
