b39ea326165742e11a347c8d7ccfc5fba3fb2f73
stuajnht <stuajnht@users.noreply.github.com>
2015-12-13 21:27:38 +0000

    Added icons to the file table cells, depending on the extension name of the file

--- HomeAccessPlus/FileTableViewCell.swift
+++ HomeAccessPlus/FileTableViewCell.swift
@@ -20,6 +20,7 @@
 //
 
 import UIKit
+import Font_Awesome_Swift
 
 class FileTableViewCell: UITableViewCell {
 
@@ -42,6 +43,94 @@ class FileTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    /// Setting the icon for the file or folder, based on what the
+    /// extention is of the file type, or a folder if empty
+    ///
+    /// To help users identify what type of a file is quickly, the
+    /// relevant icon should be shown on the table cell, before the
+    /// name and description is of the file. The icons used are part
+    /// of the FontAwesome font, as file icons do not seem to be
+    /// included in the default icons for iOS
+    ///
+    /// This data is extracted from the JSON response from the HAP+
+    /// server when a folder path is specified, and a custom option
+    /// is specified here for a network drive.
+    ///
+    /// Additional file extentions should be added to their relevant
+    /// section if they have been missed from this list (which many have!)
+    ///
+    /// - note: All file types and names are trademarks of their
+    ///         respective owners
+    ///
+    /// - author: Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+    /// - since: 0.3.0-alpha
+    /// - version: 1
+    /// - date: 2015-12-13
+    ///
+    /// - parameter fileExtension: The extension of the file of the table cell
+    func fileIcon(fileExtension: String) {
+        var icon : FAType
+        logger.debug("Setting icon for the file type: \(fileExtension)")
+        
+        // Seeing what icon should be displayed
+        switch fileExtension {
+            // Network drive
+            case "Drive":
+                icon = FAType.FAHddO
+            
+            // File folder
+            case "":
+                icon = FAType.FAFolderO
+            
+            // Adobe Acrobat documents
+            case ".pdf":
+                icon = FAType.FAFilePdfO
+            
+            // Archive documents
+            case ".zip", ".7z":
+                icon = FAType.FAFileArchiveO
+            
+            // Audio documents
+            case ".mp3", ".wav":
+                icon = FAType.FAFileAudioO
+            
+            // Code documents
+            case ".xml", ".html", ".css":
+                icon = FAType.FAFileCodeO
+            
+            // Image documents
+            case ".jpg", ".png", ".gif":
+                icon = FAType.FAFileImageO
+            
+            // Microsoft Excel documents
+            case ".xls", ".xlsx", ".csv":
+                icon = FAType.FAFileExcelO
+            
+            // Microsoft PowerPoint documents
+            case ".ppt", "pptx":
+                icon = FAType.FAFilePowerpointO
+            
+            // Microsoft Word documents
+            case ".doc", ".docx":
+                icon = FAType.FAFileWordO
+            
+            // Text documents
+            case ".txt", ".rtf":
+                icon = FAType.FAFileTextO
+            
+            // Video documents
+            case ".avi", ".mp4":
+                icon = FAType.FAFileVideoO
+            
+            // Unknown file type
+            default:
+                icon = FAType.FAFileO
+        }
+        
+        // Displaying the icon
+        imgFileIcon.setFAIconWithName(icon, textColor: UIColor(hexString: hapMainColour))
     }
 
 }
