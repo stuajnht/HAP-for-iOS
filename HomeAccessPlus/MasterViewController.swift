1e25c7a1697d4aca004742b1dca49d74c7fa694d
stuajnht <stuajnht@users.noreply.github.com>
2015-12-14 22:58:09 +0000

    Added 'back' button to the navigation bar to allow backwards navigation from the file browser table

--- HomeAccessPlus/MasterViewController.swift
+++ HomeAccessPlus/MasterViewController.swift
@@ -35,7 +35,7 @@ class MasterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
+        self.navigationItem.leftBarButtonItem = self.navigationItem.backBarButtonItem
 
         let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
         self.navigationItem.rightBarButtonItem = addButton
