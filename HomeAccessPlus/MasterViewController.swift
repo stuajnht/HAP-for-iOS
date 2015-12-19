a57a559fd9443e661d891ea25af6a520435e89e1
stuajnht <stuajnht@users.noreply.github.com>
2015-12-19 01:32:08 +0000

    Set the title of the detail view based on the name of the file selected

--- HomeAccessPlus/MasterViewController.swift
+++ HomeAccessPlus/MasterViewController.swift
@@ -264,6 +264,9 @@ class MasterViewController: UITableViewController {
                     controller.detailItem = fileType
                     controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                     controller.navigationItem.leftItemsSupplementBackButton = true
+                    // Setting the title of the detail view to be the name of the document selected
+                    // See: http://stackoverflow.com/a/14959502
+                    controller.navigationController!.navigationBar.topItem!.title = folderTitle
                 }
             }
         }
