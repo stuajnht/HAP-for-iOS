636c71191c8d08785d053080b3263ec21e5ee444
stuajnht <stuajnht@users.noreply.github.com>
2015-12-19 20:02:42 +0000

    Removed the '+' (add) button from the master view controller, as it's currently not needed, and doesn't work properly as it's based on example code from the template by Xcode

--- HomeAccessPlus/MasterViewController.swift
+++ HomeAccessPlus/MasterViewController.swift
@@ -60,12 +60,12 @@ class MasterViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationItem.leftBarButtonItem = self.navigationItem.backBarButtonItem
 
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
-        if let split = self.splitViewController {
-            let controllers = split.viewControllers
-            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
-        }
+        //let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        //self.navigationItem.rightBarButtonItem = addButton
+        //if let split = self.splitViewController {
+        //    let controllers = split.viewControllers
+        //    self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        //}
         
         // Setting the navigation bar colour
         self.navigationController!.navigationBar.barTintColor = UIColor(hexString: hapMainColour)
