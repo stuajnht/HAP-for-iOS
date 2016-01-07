9c86caf2b241164964c1eb0f9d896ce676d95169
stuajnht <stuajnht@users.noreply.github.com>
2016-01-07 20:41:03 +0000

    Moved both upload rows from the upload popover table so that they are more logically grouped

--- HomeAccessPlus/UploadPopoverTableViewController.swift
+++ HomeAccessPlus/UploadPopoverTableViewController.swift
@@ -42,12 +42,12 @@ class UploadPopoverTableViewController: UITableViewController {
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 2
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return 2
     }
 
     /*
