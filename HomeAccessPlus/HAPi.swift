2f574970b2910a23d97e45a2ec3ae5d3de96f21a
stuajnht <stuajnht@users.noreply.github.com>
2016-01-22 22:04:44 +0000

    Tidied up testing code that was used to decode the string returned by the HAP+ server when a file item is deleted

--- HomeAccessPlus/HAPi.swift
+++ HomeAccessPlus/HAPi.swift
@@ -613,38 +613,6 @@ class HAPi {
                     // hex encoded, which means it needs to be decoded
                     // first before any checks can be done to see if the
                     // file item has been successfully deleted
-                    // See: http://stackoverflow.com/a/31817292
-                    // See: http://stackoverflow.com/a/27880748
-                    // See: http://stackoverflow.com/q/27067508
-                    //let pattern = "(0x)?([0-9a-f]{2})"
-                    //let regex = try! NSRegularExpression(pattern: pattern, options: .CaseInsensitive)
-                    //let nsString = String(deletionResponse)
-                    //let matches = regex.matchesInString(String(deletionResponse), options: [], range: NSMakeRange(0,  nsString.characters.count))
-                    //logger.debug("Number of regex matches: \(matches.count)")
-                    
-                    //var collectMatches: Array<String> = []
-                    //for match in matches {
-                        //// range at index 0: full match
-                        //// range at index 1: first capture group
-                        //var substring = (String(deletionResponse) as NSString).substringWithRange(match.rangeAtIndex(0))
-                        //substring = String(Character(UnicodeScalar(UInt32(substring)!)))
-                        //logger.debug("Substring value: \(substring)")
-                        //collectMatches.append(substring)
-                    //}
-                    
-                    //logger.debug("Result from matches: \(collectMatches)")
-                    //let formattedDeletionResponse = matches.map {
-                        //Character(UnicodeScalar(UInt32(nsString.substringWithRange($0.rangeAtIndex(2)), radix: 16)!))
-                    //}
-                    //let characters = matches.map {
-                        //Character(UnicodeScalar(UInt32(nsString.substringWithRange($0.rangeAtIndex(2)), radix: 16)!))
-                    //}
-                    
-                    
-                    //let regex = try NSRegularExpression(pattern: "(0x)?([0-9a-f]{2})", options: .CaseInsensitive)
-                    //let nsString = deletionResponse as NSString
-                    //let results = regex.matchesInString(deletionResponse, options: [], range: NSMakeRange(0, nsString.length))
-                    //let formattedDeletionResponse = results.map { nsString.substringWithRange($0.range)}
                     
                     // Removing any characters from the string that shouldn't
                     // be there, namely '<', '>' and ' ', as these cause
@@ -654,31 +622,33 @@ class HAPi {
                     deletionString = deletionString.stringByReplacingOccurrencesOfString(">", withString: "")
                     deletionString = deletionString.stringByReplacingOccurrencesOfString(" ", withString: "")
                     
+                    // Converting the hex string into Unicode values, to check
+                    // that the file item from the server has been successfully
+                    // deleted
                     // See: http://stackoverflow.com/a/30795372
-                    var chars = [Character]()
+                    var deletionStringCharacters = [Character]()
                     
-                    for c in deletionString.characters
-                    {
-                        chars.append(c)
+                    for characterPosition in deletionString.characters {
+                        deletionStringCharacters.append(characterPosition)
                     }
                     
+                    // This version of Swift is different from the
+                    // hex to ascii example used above, so we need
+                    // to call a different function
                     // See: http://stackoverflow.com/a/24372631
-                    let numbers =  0.stride(to: chars.count, by: 2).map{
-                        strtoul(String(chars[$0 ..< $0+2]), nil, 16)
+                    let characterMap =  0.stride(to: deletionStringCharacters.count, by: 2).map{
+                        strtoul(String(deletionStringCharacters[$0 ..< $0+2]), nil, 16)
                     }
                     
-                    var final = ""
-                    var i = 0
+                    var decodedString = ""
+                    var characterMapPosition = 0
                     
-                    while i < numbers.count {
-                        final.append(Character(UnicodeScalar(Int(numbers[i]))))
-                        i++
+                    while characterMapPosition < characterMap.count {
+                        decodedString.append(Character(UnicodeScalar(Int(characterMap[characterMapPosition]))))
+                        characterMapPosition++
                     }
                     
-                    
-                    // Getting the output from the matches array/
-                    // See: http://stackoverflow.com/a/25827087
-                    let formattedDeletionResponse = final
+                    let formattedDeletionResponse = decodedString
                     logger.debug("Formatted response from server from deleting file item: \(formattedDeletionResponse)")
                     
                     // Seeing if the file was deleted successfully or not
