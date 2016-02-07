512bec9fab97a29ede39cc48af56be9689174a83
stuajnht <stuajnht@users.noreply.github.com>
2016-02-07 23:11:57 +0000

    Added code to the document provider, which is a direct copy of classes from the main Home Access Plus+ app, so that users can browse to files and folders

--- HomeAccessPlusDocumentProvider/DocumentPickerViewController.swift
+++ HomeAccessPlusDocumentProvider/DocumentPickerViewController.swift
@@ -20,13 +20,49 @@
 //
 
 import UIKit
+import MBProgressHUD
+import SwiftyJSON
 import XCGLogger
 
+/// This class is a direct copy of the one MasterViewController
+/// in the HomeAccessPlus group, with some functions removed
+///
+/// - author: Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+/// - since: 0.7.0-alpha
+/// - version: 1
+/// - date: 2016-02-07
 class DocumentPickerViewController: UIDocumentPickerExtensionViewController {
+    
+    // Adding a reference to the file browser table,
+    // so that it can be reloaded in the code
+    // See: http://stackoverflow.com/a/33724276
+    @IBOutlet weak var tblFileBrowser: UITableView!
     
     // Loading an instance of the HAPi class, so that the
     // functions in it can be used in the document provider
     let api = HAPi()
+    
+    // MBProgressHUD variable, so that the detail label can be updated as needed
+    var hud : MBProgressHUD = MBProgressHUD()
+    
+    /// A listing to the current folder the user is in, or
+    /// an empty string if the main drive listing is being
+    /// shown
+    /// - note: This variable should be set before the view
+    ///         controller is called in the 'prepareForSegue'
+    var currentPath = ""
+    
+    /// Array to hold the items that are shown in the table that
+    /// is used to browse the files
+    ///
+    /// - note: The order of and data stored in this array is in
+    ///         the following order:
+    ///   1. File / folder name
+    ///   2. Path to the item
+    ///   3. The type of item this is (Drive, Directory, File Type)
+    ///   4. The extension of the file, or empty if it is a directory
+    ///   5. Additional details for the file (size, modified date, etc...)
+    var fileItems: [AnyObject] = []
 
     @IBAction func openDocument(sender: AnyObject?) {
         let documentURL = self.documentStorageURL!.URLByAppendingPathComponent("Untitled.txt")
@@ -45,8 +81,309 @@ class DocumentPickerViewController: UIDocumentPickerExtensionViewController {
             logger.debug("HAP+ document provider opened for site: \(siteName)")
         }
         
-        // Checking to see if there is a connection available
-        api.checkConnection()
+        loadFileBrowser()
+    }
+    
+    
+    // MARK: MBProgressHUD
+    // The following functions look after showing the HUD during the login
+    // progress so that the user knows that something is happening. This
+    // is slightly different from the login view controller one, as we need
+    // to get it to show on top of the table view
+    // See: http://stackoverflow.com/a/26901328
+    func hudShow(detailLabel: String) {
+        hud = MBProgressHUD.showHUDAddedTo(self.navigationController!.view, animated: true)
+        hud.labelText = "Please wait..."
+        hud.detailsLabelText = detailLabel
+    }
+    
+    /// Updating the detail label that is shown in the HUD
+    ///
+    /// - author: Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+    /// - since: 0.2.0-beta
+    /// - version: 1
+    /// - date: 2015-12-08
+    /// - seealso: hudShow
+    /// - seealso: hudHide
+    ///
+    /// - parameter labelText: The text that should be shown for the HUD label
+    func hudUpdateLabel(labelText: String) {
+        hud.detailsLabelText = labelText
+    }
+    
+    // The following functions look after showing the HUD during the download
+    // progress so that the user knows that something is happening.
+    // See: http://stackoverflow.com/a/26901328
+    func hudUploadingShow() {
+        hud = MBProgressHUD.showHUDAddedTo(self.navigationController!.view, animated: true)
+        hud.detailsLabelText = "Uploading..."
+        // See: http://stackoverflow.com/a/26882235
+        hud.mode = MBProgressHUDMode.DeterminateHorizontalBar
+    }
+    
+    /// Updating the progress bar that is shown in the HUD, so the user
+    /// knows how far along the download is
+    ///
+    /// - author: Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+    /// - since: 0.5.0-alpha
+    /// - version: 1
+    /// - date: 2016-01-06
+    /// - seealso: hudShow
+    /// - seealso: hudHide
+    ///
+    /// - parameter currentDownloadedBytes: The amount in bytes that has been uploaded
+    /// - parameter totalBytes: The total amount of bytes that is to be uploaded
+    func hudUpdatePercentage(currentUploadedBytes: Int64, totalBytes: Int64) {
+        let currentPercentage = Float(currentUploadedBytes) / Float(totalBytes)
+        logger.verbose("Current uploaded percentage: \(currentPercentage * 100)%")
+        hud.progress = currentPercentage
+    }
+    
+    func hudHide() {
+        MBProgressHUD.hideAllHUDsForView(self.navigationController!.view, animated: true)
+    }
+    
+    /// Gets the list of files, folders or drives to display
+    /// then in the file browser table in the master view
+    ///
+    /// This function is called when the folder is browsed to,
+    /// to generate a list of the folders and files contained in it.
+    /// This function is also called when the folder is refreshed,
+    /// such as pulling down on the table view, deleting a file or
+    /// adding a new folder
+    ///
+    /// - author: Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+    /// - since: 0.3.0-beta
+    /// - version: 1
+    /// - date: 2015-12-19
+    func loadFileBrowser() {
+        // Hiding the built in table refresh control, as the
+        // HUD will replace the loading spinner
+        //refreshControl.endRefreshing()
+        
+        // Clearing out all items in the fileItems array, otherwise
+        // then the table is refreshed all items are added again
+        // so the table will always double in length
+        self.fileItems.removeAll()
+        
+        // Seeing if we are showing the user their available drives
+        // or if the user is browsing the folder hierarchy
+        if (currentPath == "") {
+            logger.debug("Loading the drives available to the user")
+            hudShow("Loading drives")
+            navigationItem.title = "My Drives"
+            // Getting the drives available to the user
+            api.getDrives({ (result: Bool, response: AnyObject) -> Void in
+                logger.verbose("\(result): \(response)")
+                
+                // Making sure that the request completed successfully, or
+                // let the user know if there was a problem (such as
+                // no connection to the Internet)
+                if (result) {
+                    logger.info("Successfully collected drive JSON data")
+                    let json = JSON(response)
+                    for (_,subJson) in json {
+                        let name = subJson["Name"].string
+                        let path = subJson["Path"].string
+                        // Seeing if the value returned for the drive from the HAP+
+                        // server is a minus value, which should not be displayed
+                        // See: https://github.com/stuajnht/HAP-for-iOS/issues/17
+                        var space = ""
+                        if (subJson["Space"].double! < 0) {
+                            logger.debug("Available drive space is reported as less than 0")
+                        } else {
+                            // Removing the 'optional' text displayed when
+                            // converting the space available into a string
+                            // See: http://stackoverflow.com/a/25340084
+                            space = String(format:"%.2f", subJson["Space"].double!) + "% used"
+                        }
+                        logger.debug("Drive name: \(name)")
+                        logger.debug("Drive path: \(path)")
+                        logger.debug("Drive usage: \(space)")
+                        
+                        // Creating a drive letter to show under the name of the
+                        // drive, as this will be familiar to how drives are
+                        // presented with letters on Windows
+                        let driveLetter = path!.stringByReplacingOccurrencesOfString("\\", withString: ":")
+                        
+                        // Adding the current files and folders in the directory
+                        // to the fileItems array
+                        self.addFileItem(name!, path: path!, type: driveLetter + " Drive", fileExtension: "Drive", details: space)
+                    }
+                    
+                    // Hiding the HUD and adding the drives available to the table
+                    self.hudHide()
+                    self.tblFileBrowser.reloadData()
+                } else {
+                    logger.warning("There was a problem getting the drive JSON data")
+                    self.hudHide()
+                    
+                    // The cancel and default action styles are back to front, as
+                    // the cancel option is bold, which we want the 'try again'
+                    // option to be chosen by the user
+                    let driveCheckProblemAlert = UIAlertController(title: "Unable to load drives", message: "Please check that you have logged in to the Home Access Plus+ app, then try again", preferredStyle: UIAlertControllerStyle.Alert)
+                    driveCheckProblemAlert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
+                    driveCheckProblemAlert.addAction(UIAlertAction(title: "Try Again", style: UIAlertActionStyle.Cancel, handler: {(alertAction) -> Void in
+                        self.loadFileBrowser() }))
+                    self.presentViewController(driveCheckProblemAlert, animated: true, completion: nil)
+                }
+            })
+        } else {
+            logger.debug("Loading the contents of the folder: \(currentPath)")
+            hudShow("Loading folder")
+            // Show the files and folders in the path the
+            // user has browsed to
+            //loadSampleFiles()
+            api.getFolder(currentPath, callback: { (result: Bool, response: AnyObject) -> Void in
+                logger.verbose("\(result): \(response)")
+                
+                // Making sure that the request completed successfully, or
+                // let the user know if there was a problem (such as
+                // no connection to the Internet)
+                if (result) {
+                    logger.info("Successfully collected folder JSON data")
+                    let json = JSON(response)
+                    for (_,subJson) in json {
+                        let name = subJson["Name"].string
+                        let type = subJson["Type"].string
+                        let modified = subJson["ModifiedTime"].string
+                        let fileExtension = subJson["Extension"].string
+                        let size = subJson["Size"].string
+                        let path = subJson["Path"].string
+                        let details = modified! + "    " + size!
+                        logger.verbose("Name: \(name)")
+                        logger.verbose("Type: \(type)")
+                        logger.verbose("Date modified: \(modified)")
+                        
+                        // Adding the current files and folders in the directory
+                        // to the fileItems array
+                        self.addFileItem(name!, path: path!, type: type!, fileExtension: fileExtension!, details: details)
+                    }
+                    self.hudHide()
+                    self.tblFileBrowser.reloadData()
+                } else {
+                    logger.warning("There was a problem getting the folder JSON data")
+                    self.hudHide()
+                    
+                    // The cancel and default action styles are back to front, as
+                    // the cancel option is bold, which we want the 'try again'
+                    // option to be chosen by the user
+                    let folderCheckProblemAlert = UIAlertController(title: "Unable to load folder", message: "Please check that you have a signal, then try again", preferredStyle: UIAlertControllerStyle.Alert)
+                    folderCheckProblemAlert.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: nil))
+                    folderCheckProblemAlert.addAction(UIAlertAction(title: "Try Again", style: UIAlertActionStyle.Cancel, handler: {(alertAction) -> Void in
+                        self.loadFileBrowser() }))
+                    self.presentViewController(folderCheckProblemAlert, animated: true, completion: nil)
+                }
+            })
+        }
+    }
+    
+    /// Adds the current item fetched from the JSON response to the array
+    /// of files in the directory
+    ///
+    /// To provide a standard interface to add items in the currently browsed
+    /// folder to the fileItems array, this function is called with data
+    /// so that the current item can be added
+    ///
+    /// - author: Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+    /// - since: 0.3.0-beta
+    /// - version: 1
+    /// - date: 2015-12-18
+    ///
+    /// - seealso: fileItems
+    ///
+    /// - parameter name: File / folder name
+    /// - parameter path: Path to the item
+    /// - parameter type: The type of item this is (Drive, Directory, File Type)
+    /// - parameter fileExtension: The extension of the file, or empty if it is a directory
+    /// - parameter details: Additional info for the file (size, modified date, etc...)
+    func addFileItem(name: String, path: String, type: String, fileExtension: String, details: String) {
+        // Creating an array to hold the current item that is being processed
+        var currentItem: [AnyObject] = []
+        logger.verbose("Adding item to file array, with details:\n --Name: \(name)\n --Path: \(path)\n --Type: \(type)\n --Extension: \(fileExtension)\n --Details: \(details)")
+        
+        // Adding the current item to the array
+        currentItem = [name, path, type, fileExtension, details]
+        self.fileItems.append(currentItem)
+    }
+    
+    /// Checks to see if the current item is a file or a folder/drive
+    ///
+    /// There are a number of functions in this class that perform a
+    /// different action if the current item is a file or a folder/drive
+    ///
+    /// This function checks the extension of the file that is passed to
+    /// it, and then lets the calling function know if the item is a
+    /// folder or not
+    ///
+    /// - author: Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+    /// - since: 0.3.0-alpha
+    /// - version: 2
+    /// - date: 2015-12-15
+    ///
+    /// - parameter fileType: The type of file according to the HAP+ server
+    /// - returns: Is the current item a file or a folder/drive
+    func isFile(fileType: String) -> Bool {
+        // Seeing if this is a Directory based on the file type
+        // that has been passed - issue #13
+        // The "Drive" value is looked for in any part of the
+        // fileType string, as we include the drive letter in
+        // the array value - issue #17
+        if ((fileType == "") || (fileType.rangeOfString("Drive") != nil) || (fileType == "Directory")) {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    // MARK: - Table View
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fileItems.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "FileTableViewCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! DocumentProviderFileTableViewCell
+        
+        // Fetches the current file from the fileItems array
+        let file = fileItems[indexPath.row]
+        
+        // Updating the details for the cell
+        cell.lblFileName.text = file[0] as? String
+        cell.fileIcon((file[2] as? String)!, fileExtension: (file[3] as? String)!)
+        
+        // Deciding if a disclosure indicator ("next arrow") should be shown
+        if (!isFile((file[2] as? String)!)) {
+            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        } else {
+            // Removing the disclosure indicator if the current row
+            // is a file and not a folder - issue #13
+            cell.accessoryType = UITableViewCellAccessoryType.None
+        }
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        //let row = indexPath.row //2
+        ////let section = indexPath.section//3
+        //let fileName = fileItems[row][0] //4
+        //newFolder = fileName as! String
+        //logger.debug("Folder heading to title: \(newFolder)")
+        
+        // The user has selected something, so collapse the view
+        //collapseDetailViewController = false
     }
 
 }
